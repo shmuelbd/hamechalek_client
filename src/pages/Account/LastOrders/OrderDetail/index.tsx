06161aef7963b608a8b2eaf8ed8155fff642703c
shmuelbd <shmuelbd1@gmail.com>
2023-12-11 06:00:35 +0200

    order page

--- src/pages/Account/LastOrders/OrderDetail/index.tsx
+++ src/pages/Account/LastOrders/OrderDetail/index.tsx
@@ -26,8 +26,8 @@ width: 90%;
 background-color: #fafafa;
 user-select: none;
 text-decoration: none;
-border-right: 2px dashed #5a5a5a;
-border-left: 2px dashed #5a5a5a;
+/* border-right: 2px dashed #5a5a5a;
+border-left: 2px dashed #5a5a5a; */
 height: 100vh;
 padding-top: 15px;
 `;
@@ -36,6 +36,46 @@ width: 100%;
 height: min-content;
 text-align: center;
 font-size: 15px;
+`;
+const Table = styled.div`
+width: 100%;
+display: flex;
+align-items: flex-start;
+justify-content: space-between;
+box-sizing: border-box;
+padding: 4px;
+`;
+
+const RowName = styled.div`
+width: 65%;
+`;
+const RowQuantity = styled.div`
+width: 10%;
+text-align: center;
+`;
+const RowTotal_line = styled.div`
+width: 25%;
+text-align: end;
+font-weight: 600;
+`;
+const TableTotal = styled.div`
+width: 100%;
+display: flex;
+align-items: flex-start;
+justify-content: space-between;
+box-sizing: border-box;
+margin-top: 20px;
+background-color: #d6d6d6;
+padding: 10px;
+`;
+const PReceipt_totalTitle = styled.div`
+width: 100%;
+text-align: start;
+`;
+const PReceipt_total = styled.div`
+width: 100%;
+text-align: end;
+font-weight: 600;
 `;
 
 type Props = {}
@@ -81,22 +121,27 @@ const OrderDetail = (props: Props) => {
                             <P> {order.customer_city}</P>
                             <P> {order.customer_phone}</P>
                             <P>________________________</P>
-                            <P></P>
-                            <P></P>
-                            <P></P>
+                            {
+                                order.items.map((item: any, index: number) => (
 
+                                    <Table key={index}>
+                                        <RowName>{item.description}</RowName>
+                                        <RowQuantity>{item.quantity}</RowQuantity>
+                                        <RowTotal_line>{Number(item.total_line).toFixed(2)} ₪ </RowTotal_line>
+                                    </Table>
+                                ))
+                            }
+                            <P> {order.receipt_total}</P>
+                            {/* <P> {order.documentLink}</P> */}
+                            <P></P>
+                            <P></P>
+                            <TableTotal>
+                                <PReceipt_totalTitle>סה"כ:</PReceipt_totalTitle>
+                                {/* <PReceipt_total> {order.receipt_total}</PReceipt_total> */}
+                                <PReceipt_total> {Number(order.receipt_total).toFixed(2)} ₪</PReceipt_total>
+                            </TableTotal>
                         </Content>
                     </Container>
-                // <Item>
-                //     <P>הזמנות אחרונות</P>
-
-                //     {
-                //         orders.map((item: any, index: number) => (
-                //             <OrderItem item={item} key={index} />
-                //         ))
-                //     }
-                // </Item>
-                // <Container>OrderDetail</Container>
             }
         </>
     )

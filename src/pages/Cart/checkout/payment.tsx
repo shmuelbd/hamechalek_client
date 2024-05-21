9140530ca1b45c166da45ea3750900a5d4da79e6
shmuelbd <shmuelbd1@gmail.com>
2024-05-21 04:05:52 +0300

    start implement checkout function

--- src/pages/Cart/checkout/payment.tsx
+++ src/pages/Cart/checkout/payment.tsx
@@ -1,10 +1,39 @@
+import { motion } from 'framer-motion';
 import React from 'react'
+import styled from 'styled-components';
+import "./payment.css"
+const Container = styled(motion.div)`
+width: 100%;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+`;
+const IFrame = styled.iframe`
+width: 100%;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+height: 550px;
+border: none;
+/* background-color: #ff2600; */
+&.input {
+    background-color: #ff2600;
+    color: #ff2600;
+  }
+`;
 
 type Props = {}
 
 const Payment = (props: Props) => {
     return (
-        <div>Payment</div>
+        <Container>Payment
+            <IFrame className="IFrame" src="https://testicredit.rivhit.co.il/payment/PaymentItems.aspx?GroupId=80eb70bf-8b26-4dd6-affa-499eb360a64d&Token=97b639df-5b8e-4196-a873-a29e0d3fa3b7"
+            >
+
+            </IFrame>
+
+
+        </Container>
     )
 }
 
